ec974feb120b0372f929336df4e9a2f41715eaef
houjicha14 <76984025+houjicha14@users.noreply.github.com>
2021-01-07 01:52:52 +0900

    Update main-ja.js

--- 2020web/week5/main-ja.js
+++ 2020web/week5/main-ja.js
@@ -32,8 +32,8 @@ function result() {
 
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
-    const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    newStory = newStory.replace('94 fahrenheit',temperature);
+    const temperature =  '摂氏' +Math.round((94-32) * 5 / 9) + '度';
+    newStory = newStory.replace('華氏94度',temperature);
     newStory = newStory.replace('300 pounds',weight);
   }
 
